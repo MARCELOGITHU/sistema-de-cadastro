b242fcb1679b72a65c14bff11a5e1bc694dd518a
marcelo <marcelohls0407@gmail.com>
2022-10-12 17:06:43 -0300

    voltando para uma versao anterior

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
 console.log("commit a mais");
-console.log("enviando para o github");
