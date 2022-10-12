d5a0dd019a6ecac68cb0de223145e0aa2a032744
marcelo <marcelohls0407@gmail.com>
2022-10-12 16:44:38 -0300

    adicionando o console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
 console.log("commit a mais");
+console.log("enviando para o github");
