cea96ab04aadf6b12127a99af5a2067efacadb5f
marcelo <marcelohls0407@gmail.com>
2022-10-12 16:38:26 -0300

    console log indicando modificacacoes

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log("rodando o sistema de gerenciamento de jogos")
+console.log("rodando o sistema de gerenciamento de jogos");
+console.log("aplicação está se conectando ao banco de dados");
+console.log("commit a mais");
